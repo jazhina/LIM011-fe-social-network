afc701bf1931da83ad23c34e02217626c0c469d3
jazmin <jazmin12977@gmail.com>
2019-12-06 22:37:35 +0100

    Conectando a FireBase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import { example } from './example.js';
+// INICIAR CLOUD FIRESTORE
+firebase.initializeApp({
+    apiKey: "AIzaSyDKaqE2NpTEwcOS0rWDwUuVCJ94fcApoiQ",
+    authDomain: "pet-lovers-5dca4.firebaseapp.com",
+    projectId: "pet-lovers-5dca4",
+  });
+  
+  const db = firebase.firestore();
+  
+const sesion = document.getElementById('button');
+sesion.addEventListener('click', (e) => {
+    e.preventDefault()
+    const correo = document.getElementById('e-mail').value;
+    const contraseña = document.getElementById('password').value;
+    console.log(correo);
+    console.log(contraseña);
 
-example();
+  db.collection("users").add({
+    e_mail : correo,
+    password: contraseña,
+  })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+});
+
+/*import { example } from './example.js';
+
+example();*/
