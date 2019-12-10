1f2533a69aaa95ff79b2afb17c6512a5dd4aa213
jazhina <jazmin12977@gmail.com>
2019-12-10 04:21:57 -0500

    template publicaciones

--- src/index.js
+++ src/index.js
@@ -28,7 +28,50 @@ sesion.addEventListener('click', (e) => {
         console.error("Error adding document: ", error);
     });
 });
+// PUBLICAR 
+const publicar = document.getElementById('submit');
+publicar.addEventListener('click', (e) => {
+    e.preventDefault()
+    const textarea = document.getElementById('texto').value;
+    console.log(textarea);
 
+  db.collection("publicaciones").add({
+   contenido: textarea,
+  })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        document.getElementById('texto').value = '';
+    })
+    .catch(function(error) {
+        console.error("Error: ", error);
+    });
+});
+// LISTAR PUBLICACIONES 
+const tabla = document.querySelector('#tabla');
+db.collection("publicaciones").onSnapshot((querySnapshot) => {
+    tabla.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().contenido}`);
+        tabla.innerHTML += `
+        <td scope='row'> ${doc.id}</td>
+        <td><textarea> ${doc.data().contenido}</textarea></td>
+        <td><button  onclick="eliminar('${doc.id}')" >Eliminar</button></td>
+        <td><button id='editar'>Editar</button></td>
+        `
+    });
+});
+
+// BORRAR PUBLICACION
+// const borrar = document.querySelector('#eliminar');
+// borrar.addEventListener('click', (id) => {
+function eliminar(id) {
+    db.collection ("publicaciones"). doc(id). delete(). then (function () { 
+        console.log ("¡Documento eliminado con éxito!"); 
+    }). catch (function (error) { 
+        console. error ("Error al eliminar el documento:", error); 
+    });
+}
+// });
 /*import { example } from './example.js';
 
 example();*/
