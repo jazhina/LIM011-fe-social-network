5d1a3f3dead5ed376425837133ca57f342411240
Yanira-AB <55994073+Yanira-AB@users.noreply.github.com>
2019-12-10 19:28:24 -0500

    maqueta de muro y logueo

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ const sesion = document.getElementById('button');
 const db = firebase.firestore();
 
 sesion.addEventListener('click', (e) => {
-  alert('hi');
-});
-
-sesion.addEventListener('click', (e) => {
     e.preventDefault()
     const correo = document.getElementById('e-mail').value;
     const contraseña = document.getElementById('password').value;
@@ -42,22 +38,25 @@ btnFace.addEventListener('click', (e)=> {
 
   var provider = new firebase.auth.FacebookAuthProvider();
   provider.addScope('user_birthday');
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+  firebase.auth().signInWithRedirect(provider);
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 });
 
 const btnGoogle = document.getElementById('btnGoogle');
