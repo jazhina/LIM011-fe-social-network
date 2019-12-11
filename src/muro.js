0392287eda12185306e8a92d0c926986bcc8a1de
Yanira-AB <55994073+Yanira-AB@users.noreply.github.com>
2019-12-11 12:44:02 -0500

    muro.js

--- src/muro.js
+++ src/muro.js
@@ -1,3 +1,17 @@
-// export default () => {
-//     const viewMuro = 
-// }
+/* eslint-disable linebreak-style */
+export default () => {
+  const menu = document.querySelector('#enlaces');
+  const btnMenu = document.querySelector('#menu-movil');
+  let count = 0;
+
+  btnMenu.addEventListener('click', () => {
+    if (count === 0) {
+      menu.className = ('animationTwo');
+      count = 1;
+    } else {
+      menu.classList.remove('animationTwo');
+      menu.className = ('animationOne');
+      count = 0;
+    }
+  });
+};
