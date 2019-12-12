f82a3b05dbdc4e1a1ee44ed6b2b32ae94ca5560c
KellyRoca <rocarondinel.kelly@gmail.com>
2019-12-12 18:36:31 -0500

    agrega 2 vistas SPA

--- src/example.js
+++ src/example.js
@@ -1,4 +1,5 @@
 export const example = () => {
     // aquí tu código
     
-}
+};
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import { example } from './example.js';
 
-example();
+const btnSignIn = document.getElementById('btnSignIn');
+btnSignIn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const emailSignIn = document.querySelector('#emailSignIn').value;
+  const passwordSignIn = document.querySelector('#passwordSignIn').value;
+  console.log(emailSignIn);
+  console.log(passwordSignIn);
+  firebase.auth().createUserWithEmailAndPassword(emailSignIn,
+    passwordSignIn).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+});
+
+
+// Iniciar sesión
+const btnLogIn = document.getElementById('btnLogIn');
+
+btnLogIn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const emailLogIn = document.getElementById('emailLogIn');
+  const passwordLogIn = document.getElementById('passwordLogIn');
+  firebase.auth().signInWithEmailAndPassword(emailLogIn.value,
+    passwordLogIn.value).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+    console.log(errorMessage);
+    console.log(errorCode);
+    if (errorMessage !== '') {
+      console.log('no haz iniciado sesión');
+    }
+    // ...
+  });
+});
