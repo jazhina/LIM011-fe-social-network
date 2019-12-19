ebf938079167d7c9918500a83e875ae4c51677b2
Yanira-AB <arenazas2222@gmail.com>
2019-12-19 15:06:33 -0500

    obteniendo datos face y guardarlo a firestore 19/12

--- SPA/main.js
+++ SPA/main.js
@@ -12,24 +12,6 @@ import { initFire } from './functions/auth-firebase.js';
     changeView(window.location.hash);
     window.addEventListener('hashchange', ()=> changeView(window.location.hash));
     initFire();
-
-const init = () => {
-  // Initialize Firebase
-  const config = {
-    apiKey: 'AIzaSyDKaqE2NpTEwcOS0rWDwUuVCJ94fcApoiQ',
-    authDomain: 'pet-lovers-5dca4.firebaseapp.com',
-    databaseURL: 'https://pet-lovers-5dca4.firebaseio.com',
-    projectId: 'pet-lovers-5dca4',
-    storageBucket: 'pet-lovers-5dca4.appspot.com',
-    messagingSenderId: '436468793619',
-    appId: '1:436468793619:web:358f143ed53c4732bc9ae6',
-    measurementId: 'G-EDJD24K5SD',
   };
-
-  firebase.initializeApp(config);
-  // Finaliza firebase
-  changeView(window.location.hash);
-  window.addEventListener('hashchange', () => changeView(window.location.hash));
-};
 
 window.addEventListener('load', init);
