3a59bb7d3140bdd6929fc1c0c88e22d47dee0132
jazhina <jazmin12977@gmail.com>
2019-12-19 23:00:20 +0100

    Guarda y lista publicaciones

--- SPA/view/catalogo.js
+++ SPA/view/catalogo.js
@@ -1,6 +1,7 @@
 import { menuAnimation } from '../functions/animation.js';
 
 export default () => {
+  const db = firebase.firestore();
   const viewCatalogo = `
     <header class="header-movil">
     <menu id="menu-movil" class="menu-movil"><i class="fas fa-bars fa-2x bars"></i></menu>
@@ -45,10 +46,10 @@ export default () => {
     </section>
     <section class="section-publics-muro">
       <form class="form">
-        <textarea placeholder="¿Qué quieres compartir?" name="" id="" cols="37" rows="4"></textarea>
+        <textarea id = "texto" placeholder="¿Qué quieres compartir?" name="" id="" cols="37" rows="4"></textarea>
         <div class="btn-coment">
             <button class="btn-img"><i class="far fa-image icons-white"></i></button>
-            <button class="btn-share">Compartir</button>
+            <button class="btn-share" id = "compartir">Compartir</button>
         </div>
       </form>
       <div class="coment">
@@ -61,24 +62,68 @@ export default () => {
           <button class="btns-note"><i class="fas fa-share icons-white"></i></button>
         </div>
       </div>
-      <div class="coment">
-        <div class="title-note">
-          <p>Publicado por Jean Cedron - Comunal</p><i class="fas fa-times"></i>
-        </div>
-        <p class="text-coment">Hola a todos!</p>
-        <div class="section-btns-note">
-          <button class="btns-note"><i class="far fa-grin-hearts icons-white"></i></button>
-          <button class="btns-note"><i class="fas fa-share icons-white"></i></button>
-        </div>
+      <div id="comentarios" class = "coment">
       </div>
     </section>
+    <div id="perfil">
   </main>
   <script type="module" src="animation.js"></script>
   `;
 
   const divElement = document.createElement('div');
   divElement.innerHTML = viewCatalogo;
+  // PUBLICAR 
+  const publicar = divElement.querySelector('#compartir');
+ publicar.addEventListener('click', (e) => {
+     e.preventDefault()
+     const textarea = divElement.querySelector('#texto').value;
+     console.log(textarea);
 
+   db.collection("publicaciones").add({
+    contenido: textarea,
+   })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        divElement.querySelector('#texto').value = '';
+     })
+     .catch(function(error) {
+        console.error("Error: ", error);
+     });
+});
+// LISTAR PUBLICACIONES 
+const comentarios = divElement.querySelector('#comentarios');
+ db.collection("publicaciones").onSnapshot((querySnapshot) => {
+  comentarios.innerHTML = '';
+   querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().contenido}`);
+        comentarios.innerHTML += `
+          <div class = "comment">
+            <div class="title-note">
+            <p>Publicado por Jean Cedron - Comunal</p><i class="fas fa-times"></i>
+            </div>
+              <p class="text-coment">${doc.data().contenido}</p>
+            <div class="section-btns-note">
+              <button class="btns-note" id = "eliminar" ><i class="far fa-grin-hearts icons-white"></i></button>
+              <button class="btns-note"><i class="fas fa-share icons-white"></i></button>
+            </div>
+          </div>
+         `
+     });
+});
+
+/* // PRUEBA PERFIL 
+const perfil = divElement.querySelector('#perfil');
+db.collection("users").onSnapshot((querySnapshot) => {
+  perfil.innerHTML = '';
+   querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().email}`);
+        perfil.innerHTML += `
+              <p>${doc.data().email}</p>
+              <p>${doc.data().name}</p>
+              <img class="photo" src=${doc.data().photo}/>
+         `
+     });
+}); */
   // Funciones
   const menuMovil = divElement.querySelector('#menu-movil');
   menuMovil.addEventListener('click', menuAnimation);
